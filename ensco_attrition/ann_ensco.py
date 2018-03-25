0c8c8abc25ea1078ae47011b7894fcdeeed9aa35
vrcrush <35403901+vrcrush@users.noreply.github.com>
2018-03-25 17:16:21 -0500

    Add files via upload
    Predict Employee exits the company

---
+++ ensco_attrition/ann_ensco.py
@@ -0,0 +1,93 @@
+# Artificial Neural Network
+
+#Predict if Ensco employee will stay
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('ensco2.csv')
+X = dataset.iloc[:, 0:9].values
+y = dataset.iloc[:, 9].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+X[:, 1] = labelencoder_X.fit_transform(X[:, 1])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+#X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Part 2 - Making the ANN
+
+# Importing the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 8, kernel_initializer = 'uniform', activation = 'relu', input_dim = 12))
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 8, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dropout(0.15))
+
+# Adding the third hidden layer
+classifier.add(Dense(units = 8, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dropout(0.15))
+
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 2000)
+
+# Part 3 - Making predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Predicting a single new observation
+"""Predict if the EMPLOYEE with the following informations will leave the bank:
+BU: CORP
+BIRTH: US
+Grade: 35
+department: 9940
+Last Hire date
+Birth Date
+Comp Rate: 5000
+Last Increase date
+Employee Type: Ecip/LTI
+"""
+
+"""Will the employee Exit?"""
+new_prediction = classifier.predict(sc.transform(np.array([[1.0,0.0,0.0,0.0,1.0, 35, 9940, 30000, 23000, 5000, 42000, 5]])))
+new_prediction = (new_prediction > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
