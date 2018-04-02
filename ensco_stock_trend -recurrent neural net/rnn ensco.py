ecc65b3f02cd4fd9fc3494f7d60a6e50fe4726d7
vrcrush <35403901+vrcrush@users.noreply.github.com>
2018-04-02 10:44:48 -0500

    Update rnn ensco.py

--- ensco_stock_trend -recurrent neural net/rnn ensco.py
+++ ensco_stock_trend -recurrent neural net/rnn ensco.py
@@ -21,7 +21,7 @@ training_set_scaled = sc.fit_transform(training_set)
 # Creating a data structure with 60 timesteps and 1 output
 X_train = []
 y_train = []
-for i in range(60, 1258):
+for i in range(60, 2019):
     X_train.append(training_set_scaled[i-60:i, 0])
     y_train.append(training_set_scaled[i, 0])
 X_train, y_train = np.array(X_train), np.array(y_train)
